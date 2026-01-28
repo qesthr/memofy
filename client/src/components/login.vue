28c569abbc6bd4427076d01116cc35b7ae6d1d56
Queen de los Reyes <queennina.dlr@gmail.com>
2026-01-28 10:11:41 +0800

    connected vue + laravel

---
+++ client/src/components/login.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import api from '@/services/api'
+
+const email = ref('')
+const password = ref('')
+
+const login = async () => {
+  try {
+    const res = await api.post('/login', {
+      email: email.value,
+      password: password.value
+    })
+    localStorage.setItem('token', res.data.token)
+    console.log('Logged in', res.data.user)
+  } catch (err) {
+    alert('Login failed')
+  }
+}
+</script>
